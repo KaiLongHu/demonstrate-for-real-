9ea9b1a4a9088c754c4c860867736b43927390ec
LTXB <781802887@qq.com>
2021-04-17 22:53:30 +0800

    写完前两个always块

--- switch_ctrl.v
+++ switch_ctrl.v
@@ -1,3 +1,4 @@
+
 //轮询控制
 module switch_ctrl(
 input 	clk,//时钟50M
@@ -14,127 +15,158 @@ output	[7:0]  q//读数据
 
 //轮询间隔时间和次数可以设置
 //parameter delay_time=32'd5_000_000;//time=delay_time*20ns
-parameter delay_time=32'd50_00_000;//time=delay_time*10ns
-parameter cycle_times=8'd6;//轮询5次
 
+parameter 	delay_time	=32'd5_000_000;//time=delay_time*20ns
+parameter 	cycle_times	=8'd1;//轮询cycle_times次
+parameter 	addressnum	=5'd31;//address total number
+	
+	
+	
+reg [7:0] 	cyc_times=8'd0;		//轮询次数
+reg [31:0]	delay_cnt=32'd0;	//间隔时间计数
+reg [4:0] 	address=5'd0;		//地址
+reg 		endflag=1'b0;		//完成标识
 
-reg [7:0] times=8'd0;//轮询次数
-reg [31:0] delay_cnt=32'd0;//间隔时间计数
-reg [4:0] address=5'd0;
+reg [1:0]	flag_j1=1'd0;		//jugde1的标识
+
+reg [3:0]	NS,CS;	
+
 
 assign addr=address;
 
-reg [3:0] state=4'd0;
-parameter s_idle=4'd0;
-parameter s_start=4'd1;
-parameter s_delay=4'd2;
-parameter s_getAD=4'd3;
-parameter s_next=4'd4;
-parameter s_end=4'd5;
-parameter s_wr_0d=4'd6;
-parameter s_wr_0a=4'd7;
+parameter [3:0]
+	IDLE		=	4'd0,	//IDLE
+	s_start		=	4'd1,	//收到FLAG后启动
+	s_delay		=	4'd2,	//延迟
+	s_getAD		=	4'd3,	//获得AD值
+	s_addrpp		=	4'd4,	//地址累加
+	s_cyclepp	=	4'd5,	//判断cycle是否累加
+	
+	s_judge1w	=	4'd6,	//给一个judgefalg运算缓冲
+	s_judge1		=	4'd7,	//判断当前循环是否进行完毕,是去下一cycle 还是end
+	s_nextcyc	=	4'd8,	//进入下一cycle,addr清0
+	s_endflag	=	4'd9,	//确定cycle都执行完毕
+	s_nextaddr	=	4'd10,	//还在cycle内采样下一addr
+	
+	s_wr0d		=	4'd11,	//写0d
+	s_wr0a		=	4'd12,	//写0a,无论还有没有剩余cycle都发送已完成cycle的内容
+	s_judge2		=	4'd13;	//根据endflag判断是否还有下一cycle
+	
+	// s_end 		=	4'd13;	//end 所有内容清0
+	
 
-always@(posedge clk or negedge reset_n)
-	if(reset_n==0)//复位，低电平有效
-		state<=s_idle;
+
+
+//1ST
+always	@(posedge clk or negedge reset_n)
+	if(!reset_n)
+		CS=IDLE;
 	else
-		case(state)
-			s_idle:
-				if(flag==0)//启动flag，高电平有效
-					state<=s_start;
-				else
-					state<=s_idle;
-			s_start://开始轮询
-				state<=s_delay;//延迟
-			s_delay:
-				if(delay_cnt>=delay_time)//延迟
-					state<=s_getAD;
-				else
-					state<=s_delay;//延迟
-			s_getAD://取AD值
-				if(address==5'd31 && times>=cycle_times)
-					state<=s_end;
-				else
-					state<=s_next;
-			s_next://轮询下一地址
-				state<=s_start;
-			s_end:
-				state<=s_wr_0d;//写0d
-			s_wr_0d:
-				state<=s_wr_0a;//写0a
-			s_wr_0a:
-				state<=s_idle;
-			default:;
-		endcase
-				
-//间隔时间计数					
-always@(posedge clk or negedge reset_n)
-	if(reset_n==0)//复位，低电平有效
-		delay_cnt<=32'd0;
-	else				
-		if(state==s_delay)		
-			delay_cnt<=delay_cnt+1;
+		CS=NS;
+	
+//2ST
+always	@(posedge clk or negedge reset_n)
+	begin 
+	NS=4'dx;
+	case(CS)
+	IDLE://IDLE
+		if(flag==0)//启动采样
+			NS=s_start;
 		else
-			delay_cnt<=32'd0;
-
-reg AD_wr_en=0;
-reg [7:0] FIFO_data;
-always@(posedge clk or negedge reset_n)
-	if(reset_n==0)begin//复位，低电平有效
-		AD_wr_en<=0;
-		FIFO_data<=8'd0;
-		end
-	else				
-		if(state==s_getAD)begin
-			AD_wr_en<=1;//AD fifo写使能
-			FIFO_data<=data_out;//data_out
-			end
-		else if(state==s_wr_0d)begin//写0d
-			AD_wr_en<=1;//AD fifo写使能
-			FIFO_data<=8'h0d;//
-			end
-		else if(state==s_wr_0a)begin//写0a
-			AD_wr_en<=1;//AD fifo写使能
-			FIFO_data<=8'h0a;//
-			end
-		else
-			AD_wr_en<=0;
+			NS=IDLE;
 			
-always@(posedge clk or negedge reset_n)
-	if(reset_n==0)//复位，低电平有效
-		address<=5'd0;
-	else				
-		if(state==s_next)		
-			address<=address+5'd1;//地址累加
-		else if(state==s_start && address==5'd32)
-			address<=5'd0;
-		else
-			address<=address;			
 			
-always@(posedge clk or negedge reset_n)
-	if(reset_n==0)//复位，低电平有效
-		times<=8'd0;
-	else				
-		if(state==s_start && address==5'd32)		
-			times<=times+8'd1;//轮询次数累加
-		else
-			times<=times;				
-			
-//FIFO 8bit 4096深度		
-
-wire [15:0] usedw;	
-AD_FIFO	AD_FIFO_i (
-	.clock ( clk ),
-	.data ( FIFO_data ),
-	.rdreq ( rdreq ),
-	.wrreq ( AD_wr_en ),
-	.empty ( empty ),
-	.full ( full ),
-	.q ( q ),
-	.usedw ( usedw )
-	);			
-			
-//0d 0a
-
+	s_start://收到FLAG后启动
+		NS=s_delay;	
 		
+	s_delay://延迟
+		NS=s_getAD;
+	
+	s_getAD://获得AD值
+		NS=s_addrpp;
+	
+	s_addrpp://地址累加
+		NS=s_cyclepp;
+
+	s_cyclepp://判断cycle是否累加
+		NS=s_judge1;			
+				
+	s_judge1w://给一个judgefalg运算缓冲
+		NS=s_judge1;
+			
+	s_judge1:	//判断当前循环是否进行完毕,是去下一cycle 还是end
+				begin
+		if(flag_j1==2'd0)
+			NS=s_nextaddr;
+		if(flag_j1==2'd1)
+			NS=s_nextcyc;
+		if(flag_j1==2'd2)
+			NS=s_endflag;
+				end 
+	
+	s_nextaddr://还在cycle内采样下一addr
+		NS=s_delay;
+	
+	s_nextcyc://进入下一cycle,addr清0
+		NS=s_wr0d;
+	
+	s_endflag://确定cycle都执行完毕
+		NS=s_wr0d;
+			
+	s_wr0d://写0d
+		NS=s_wr0a;
+		
+	s_wr0a://写0a,无论还有没有剩余cycle都发送已完成cycle的内容
+		NS=s_judge2;
+	
+	s_judge2://根据endflag判断是否还有下一cycle
+			begin
+		if(endflag==1'b0)
+			NS=s_delay;
+		if(endflag==1'b1)
+			NS=IDLE;
+			end 
+	
+	
+	
+	
+	
+	
+	
+	endcase
+	end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 endmodule
+
